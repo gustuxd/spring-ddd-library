daccba9538478c00f090c938c87ec1f861642239
gustuxd <gustavojsb@hotmail.com>
2024-07-14 23:53:56 -0300

    init project

---
+++ src/test/java/com/library/LibraryApplicationTests.java
@@ -0,0 +1,19 @@
+package com.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void verifyModules() {
+		ApplicationModules.of(LibraryApplication.class).verify();
+	}
+
+}
